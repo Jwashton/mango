af94ea3ae94e2d2852abcc921f9e123c1c98f661
William Ashton <William@AshtonFam.org>
2021-01-31 10:58:57 -0500

    Update Webpack and friends

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,21 +1,13 @@
 const path = require('path');
 const glob = require('glob');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('css-minimizer-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => {
   const devMode = options.mode !== 'production';
 
   return {
-    optimization: {
-      minimizer: [
-        new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode }),
-        new OptimizeCSSAssetsPlugin({})
-      ]
-    },
     entry: {
       'app': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
     },
@@ -46,8 +38,13 @@ module.exports = (env, options) => {
     },
     plugins: [
       new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-      new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
-    ]
-    .concat(devMode ? [new HardSourceWebpackPlugin()] : [])
+      new CopyWebpackPlugin({ patterns: [{ from: 'static/', to: '../' }] })
+    ],
+    optimization: {
+      minimizer: [
+        '...',
+        new OptimizeCSSAssetsPlugin({})
+      ]
+    },
   }
 };
