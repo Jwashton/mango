2d70f20e770a7a34cac1309f27f7ff097eeeab16
William Ashton <William@AshtonFam.org>
2021-03-16 18:55:58 -0400

    Remove stale comment

--- lib/auroville/residence_service.ex
+++ lib/auroville/residence_service.ex
@@ -1,16 +1,5 @@
 defmodule Auroville.ResidenceService do
   def list_areas do
     ["Area 1", "Area 2", "Area 3"]
-    # [
-    #   "Grass Land",
-    #   "Desert Land",
-    #   "Water Land",
-    #   "Giant Land",
-    #   "Sky Land",
-    #   "Ice Land",
-    #   "Pipe Land",
-    #   "Dark Land",
-    #   "Warp Zone"
-    # ]
   end
 end
